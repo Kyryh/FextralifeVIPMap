0286372627b32aaae30f84f74e24d975274c646c
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-07-26 22:24:07 +0200

    Fixed the sidebar breaking after reinitializing

--- scripts/patcher.js
+++ scripts/patcher.js
@@ -1,9 +1,15 @@
 // Set the vip flag
 iiv = true
 
+// Store the map's sidebar
+let sidebar = document.getElementById("sidebar").outerHTML
+
 // Remove the initialized map
 map.off()
 map.remove()
+
+// Re-add the sidebar
+map._container.innerHTML = sidebar
 
 // Re-initialize the interactive map
 init()
